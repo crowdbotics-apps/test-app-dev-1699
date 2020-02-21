b6991c6e3d4c9d96f98d56590aef905ff84d53ec
Crowdbotics <team@crowdbotics.com>
2020-02-21 06:45:40 +0000

    Update models

--- backend/users/migrations/0004_testing_test.py
+++ backend/users/migrations/0004_testing_test.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0003_testing'),
+        ("users", "0003_testing"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='testing',
-            name='test',
+            model_name="testing",
+            name="test",
             field=models.CharField(blank=True, max_length=256, null=True),
         ),
     ]
